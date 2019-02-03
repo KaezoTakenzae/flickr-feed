3fc9febd060804dcf01cd5d2cbe1801114c9643d
Kyle Moxham <kyle.moxham@built.co.uk>
2019-02-03 21:33:34 +0000

    Add in routes for search and photo page

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,18 @@ import Layout from './hoc/Layout';
 import { Switch, Route } from 'react-router-dom';
 
 import Home from './components/Home';
+import Search from './components/search';
+import PhotoPage from './components/photoPage';
 
 const Routes = (props) => {
     return (
       <Layout>
         <Switch>
             <Route exact path='/' component={Home}/>
+            <Route exact path='/search/' component={Search}/>
+            <Route exact path='/search/:query' component={Search}/>
+            <Route exact path='/photo/' component={PhotoPage}/>
+            <Route exact path='/photo/:id' component={PhotoPage}/>
         </Switch>
       </Layout>
     )
