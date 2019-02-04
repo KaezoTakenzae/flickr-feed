33a41bdc1fa93947ae7628d44959b5b8444b81f4
Kyle Moxham <kyle.moxham@built.co.uk>
2019-02-04 18:04:03 +0000

    Add abort controller polyfill

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'babel-polyfill';
+import 'abortcontroller-polyfill/dist/polyfill-patch-fetch';
 import './sass/app.scss';
 
 import { BrowserRouter } from 'react-router-dom';
