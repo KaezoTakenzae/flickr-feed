6d471c74addb7dc15058e7da4fb8cfbf2564dbab
Kyle Moxham <kyle.moxham@built.co.uk>
2019-02-04 17:53:53 +0000

    Fix author link

--- src/components/photocard/PhotoCardAuthorTitle.js
+++ src/components/photocard/PhotoCardAuthorTitle.js
@@ -3,7 +3,7 @@ import React from 'react';
 const PhotoCardAuthorTitle = ({card}) => {
   return (
     <div className="photo-card-author-title">
-      <a rel='noopener noreferrer' target='_blank' href={card.link} title={card.title}>
+      <a rel='noopener noreferrer' target='_blank' href={`https://www.flickr.com/photos/${card.owner}/${card.id}/`} title={card.title}>
         {`${card.title.substr(0, 20)}${card.title.length > 20 ? '...' : ''}`}
       </a> by <a
         rel='noopener noreferrer'
